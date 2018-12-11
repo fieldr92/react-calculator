1412326f23b9ec41e4152e1c007aaf2fcd901252
fieldr <fieldr92@gmail.com>
2018-12-11 10:55:52 +0000

    ♻️ Change App component to function

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,13 @@ import TitleBar from './TitleBar/TitleBar';
 import Calculator from './Calculator/Calculator';
 import '../styles/App.css';
 
-class App extends React.Component {
-
-  render() {
-
-    return (
-      <>
-        <TitleBar />
-        <Calculator />
-      </>
-    )
-  }
+const App = () => {
+  return (
+    <>
+      <TitleBar />
+      <Calculator />
+    </>
+  )
 }
 
 export default App;
